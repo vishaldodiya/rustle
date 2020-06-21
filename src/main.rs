76633be4b15a36bd1997cb13d0a8cefc911c1252
Vishal Dodiya <vdodiya03@gmail.com>
2020-06-21 11:19:18 -0700

    sample format and simple md parser

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,26 @@
-use std::env;
-use std::fs;
+fn get_title() -> String {
+    let mut the_title = String::from(env!("CARGO_PKG_NAME"));
+    the_title.push_str(" (v");
+    the_title.push_str(env!("CARGO_PKG_VERSION"));
+    the_title.push_str("), ");
+    the_title.push_str(env!("CARGO_PKG_DESCRIPTION"));
 
-struct Program {
-    token: i64,
-    src: String,
-    oldsrc: String,
-    poolsize: i64,
-    line: i64
+    return the_title;
 }
 
-impl Program {
-    fn next(&self) {
-        return;
-    }
-
-    fn expression(&self, level: i64) {
-
-    }
-
-    fn program(&self) {
-        &self.next();
-        while self.token > 0 {
-            println!("Token is: {}", self.token);
-            &self.next();
-        }
-    }
-    
-    fn eval(&self) -> i32 {
-        return 0;
-    }
+fn print_short_banner() {
+    println!("{}", get_title());
 }
 
-pub fn run() -> i32 {
-    let args: Vec<String> = env::args().collect();
-    let index: i32;
-    let mut file_data: String;
-    let program = Program{ token: 0, src: String::from(""), oldsrc: String::from(""), poolsize: 256 * 1024, line: 1};
+fn print_long_banner() {
+    print_short_banner();
+    println!("Written by: {}\nGithub: {}\nUsage: rustle <example>.md\n", env!("CARGO_PKG_AUTHORS"), env!("CARGO_PKG_HOMEPAGE"));
+}
 
-    file_data = fs::read_to_string(&args[1])
-        .expect("Something went wrong reading file.");
-
-    file_data.push('0');
-    
-    program.program();
-    return program.eval();
+fn usage() {
+    print_long_banner()
 }
 
 fn main() {
-    run();
+    usage()
 }
