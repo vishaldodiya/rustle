8507a3136cd3d180f28d6f2cf79839d84cf41820
Vishal Dodiya <vdodiya03@gmail.com>
2020-06-28 23:53:18 -0700

    add lalrpop simple heading parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use std::path::Path;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::io::Write;
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub md);
+
+#[test]
+fn md() {
+    // Test H1 Tag.
+    assert_eq!(md::MarkDownParser::new().parse("# Hello").unwrap(), "<h1>Hello</h1>");
+
+    // Test H2 Tag.
+    assert_eq!(md::MarkDownParser::new().parse("## Hello").unwrap(), "<h2>Hello</h2>");
+}
 
 fn get_title() -> String {
     let mut the_title = String::from(env!("CARGO_PKG_NAME"));
@@ -27,7 +40,7 @@ fn usage() {
 
 fn parse_markdown_file(_filename: &str) {
     print_short_banner();
-    println!("[ INFO ] Trying to parse {}...", _filename);
+    println!("[ INFO ] {} Parsing Starting!", _filename);
 
     // Create path variable for the input file.
     let input_filename = Path::new(_filename);
@@ -74,7 +87,7 @@ fn parse_markdown_file(_filename: &str) {
                 }
 
                 output_line.push_str(&line_contents);
-            } 
+            }
         }
 
         if _ptag {
@@ -91,6 +104,17 @@ fn parse_markdown_file(_filename: &str) {
             tokens.push(output_line);
         }
     }
+
+    let mut output_filename = String::from(&_filename[.._filename.len()-3]);
+    output_filename.push_str(".html");
+
+    let mut outfile = File::create(output_filename).expect("[ ERROR ] Could not create output file!");
+
+    for line in &tokens {
+        outfile.write(line.as_bytes()).expect("[ ERROR ] Could not write output file!");
+    }
+
+    println!("[ INFO ] Parsing Complete!")
 }
 
 fn main() {
